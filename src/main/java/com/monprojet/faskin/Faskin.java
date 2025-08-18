15b347343195899128d9c67ba59591d027fddc1a
GordoxGit <romain.begot@gmail.com>
2025-08-18 21:48:03 +0200

    feat: add MySQL connection and clean build warnings

--- src/main/java/com/monprojet/faskin/Faskin.java
+++ src/main/java/com/monprojet/faskin/Faskin.java
@@ -1,19 +1,29 @@
 package com.monprojet.faskin;
 
+import com.monprojet.faskin.database.MySQLManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Faskin extends JavaPlugin {
 
+    private MySQLManager mySQLManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        getLogger().info("Faskin a été activé. En attente de la logique d'initialisation...");
-        getLogger().info("Ce plugin nécessite ProtocolLib pour fonctionner correctement.");
+        // Créer le fichier config.yml par défaut s'il n'existe pas
+        saveDefaultConfig();
+
+        // Initialiser et connecter à la base de données
+        this.mySQLManager = new MySQLManager(this);
+        this.mySQLManager.connect();
+
+        getLogger().info("Faskin a été activé.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
-        getLogger().info("Faskin a été désactivé. En attente de la logique de nettoyage...");
+        if (this.mySQLManager != null) {
+            this.mySQLManager.disconnect();
+        }
+        getLogger().info("Faskin a été désactivé.");
     }
 }
